a5bbe3744b7aaa4725819be5f0d8fde7c13c28bc
melerokristy <165970194+melerokristy@users.noreply.github.com>
2024-04-17 17:22:05 -0500

    added unit changed unit of measure

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ function formatDate(date) {
 
 function searchCity(city) {
   let apiKey = "b2a5adcct04b33178913oc335f405433";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=imperal`;
   axios.get(apiUrl).then(refreshWeather);
 }
 
