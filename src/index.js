51f5cba4e3debe7f063d53f795f8c3426d92a452
melerokristy <165970194+melerokristy@users.noreply.github.com>
2024-04-17 16:41:39 -0500

    updated ipa key

--- src/index.js
+++ src/index.js
@@ -40,7 +40,7 @@ function formatDate(date) {
 }
 
 function searchCity(city) {
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "ee43b7t37do2fbab0535cbaa4d610fad";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
   axios.get(apiUrl).then(refreshWeather);
 }
