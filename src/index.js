f248757963f2268ccda8b4315afd617a63c65252
melerokristy <165970194+melerokristy@users.noreply.github.com>
2024-04-17 17:10:04 -0500

    updated api functions

--- src/index.js
+++ src/index.js
@@ -43,49 +43,25 @@ function formatDate(date) {
 function searchCity(city) {
   let apiKey = "ee43b7t37do2fbab0535cbaa4d610fad";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&unit=imperial`;
-  axios.get(apiUrl).then(displayForecast);
+  axios.get(apiUrl).then(refreshWeather);
 }
 
 function displayForecast(response) {
   let forecastHtml = "";
+}
 
-  function handleSearchSubmit(event) {
-    event.preventDefault();
-    let searchInput = document.querySelector("#search-form-input");
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
 
-    searchCity(searchInput.value);
-  }
+  searchCity(searchInput.value);
+}
 
-  function formatDay(timestamp) {
-    let date = new Date(timestamp * 1000);
-    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-    return days[date.getDay()];
-  }
-  response.data.daily.forEach(function (day, index) {
-    if (index < 5) {
-      forecastHtml =
-        forecastHtml +
-        `
-      <div class="weather-forecast-day">
-        <div class="weather-forecast-date">${formatDay(day.time)}</div>
-
-        <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
-        <div class="weather-forecast-temperatures">
-          <div class="weather-forecast-temperature">
-            <strong>${Math.round(day.temperature.maximum)}º</strong>
-          </div>
-          <div class="weather-forecast-temperature">${Math.round(
-            day.temperature.minimum
-          )}º</div>
-        </div>
-      </div>
-    `;
-    }
-  });
-
-  let forecastElement = document.querySelector("#forecast");
-  forecastElement.innerHTML = forecastHtml;
+  return days[date.getDay()];
 }
 
 let searchFormElement = document.querySelector("#search-form");
