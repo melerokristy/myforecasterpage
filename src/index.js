ff24008b19e6aa7c2d700be14d697b1ea6a9e9a3
melerokristy <165970194+melerokristy@users.noreply.github.com>
2024-04-17 16:44:52 -0500

    updated upi url

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ function formatDate(date) {
 
 function searchCity(city) {
   let apiKey = "ee43b7t37do2fbab0535cbaa4d610fad";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&unit=imperial`;
   axios.get(apiUrl).then(refreshWeather);
 }
 
